2b085fa5e5829a509539ff5794cd6cda7a4122dc
zacva <zacva@DESKTOP-VAPOPTV>
2022-07-14 15:39:08 +0300

    Добавьте файлы проекта.

---
+++ Data/OrderContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VerstaTestVatnik.Models;
+
+namespace VerstaTestVatnik.Data
+{
+    public class OrderContext : DbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,23 @@
+﻿namespace VerstaTestVatnik.Models
+{
+    public class Order
+    {
+        int Id { get; set; }
+        string SenderCity { get; set; }
+        string SenderAddress { get; set; }
+        string RecepientCity { get; set; }
+        string RecepientAddress { get; set; }
+        float Weight { get; set; }
+        DateOnly Date { get; set; }
+
+        Order(string senderCity, string senderAddress, string recepientCity, string recepientAddress, float weight, DateOnly date)
+        {
+            SenderCity = senderCity;
+            SenderAddress = senderAddress;
+            RecepientCity = recepientCity;
+            RecepientAddress = recepientAddress;
+            Weight = weight;
+            Date = date;
+        }
+    }
+}
