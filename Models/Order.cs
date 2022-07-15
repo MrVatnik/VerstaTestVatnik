a6d49e9a389727818cb5cdbb82bd88da1d5b93b7
Valery Vatnik <zacvars@gmail.com>
2022-07-15 17:31:54 +0300

    Added views and controlleras, small changes requared

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace VerstaTestVatnik.Models
@@ -7,15 +8,34 @@ namespace VerstaTestVatnik.Models
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Key]
+        [DisplayName("Номер заказа")]
         public int Id { get; set; }
+
+        [DisplayName("Город отправителя")]
+        [Required(ErrorMessage = "Город отправителя должен быть введён!")]
         public string SenderCity { get; set; }
+        
+        [DisplayName("Адрес отправителя")]
+        [Required(ErrorMessage = "Адрес отправителя должен быть введён!")]
         public string SenderAddress { get; set; }
+
+        [DisplayName("Город получателя")]
+        [Required(ErrorMessage = "Город получателя должен быть введён!")]
         public string RecepientCity { get; set; }
+
+        [DisplayName("Адрес получателя")]
+        [Required(ErrorMessage = "Адрес получателя должен быть введён!")]
         public string RecepientAddress { get; set; }
-        public float Weight { get; set; }
+
+        [DisplayName("Вес")]
+        [Required(ErrorMessage = "Вес должен быть введён!")]
+        [DisplayFormat(DataFormatString = "{0:F2}", ApplyFormatInEditMode = true)]
+        public decimal Weight { get; set; }
 
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [DisplayName("Дата")]
+        [Required(ErrorMessage = "Дата должна быть введена!")]
         public DateTime Date { get; set; }
 
     }
